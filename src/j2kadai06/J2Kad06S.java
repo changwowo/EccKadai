183f7c7b47eb291b1e4ed42ebbd6e9d2ff454429
changwoo LEE <ckddn1324@gmail.com>
2022-10-17 12:24:13 +0900

    fiarjk06

--- src/j2kadai06/J2Kad06S.java
+++ src/j2kadai06/J2Kad06S.java
@@ -3,6 +3,7 @@ package j2kadai06;/*
 	作成日：2022/10/17
 	作成者：田中太郎
 */
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class J2Kad06S {
@@ -12,70 +13,40 @@ public class J2Kad06S {
         System.out.println("20個ある石を交互に取っていきます。一度に取れる石の数は1-3個です。");
         System.out.println("最後の1つを取った方が負けです。");
         System.out.println();
-
         // 初期設定
         int	stone = 20;			// 石の数
 
-//        UserPlayer user = new UserPlayer();
-//        CompPlayer comp = new CompPlayer();
-        BasePlayer[] basePlayers = new BasePlayer[4];
-        BasePlayer basePlayer ;
-        BasePlayer userPlayer ;
-        BasePlayer masterPlayer ;
-        BasePlayer compPlayer;
-
-
+        BasePlayer[] basePlayers = {new BasePlayer(), new BasePlayer() , new CompPlayer() , new MasterPlayer()};
+        ArrayList<BasePlayer> basePlayers1 = new ArrayList<>();
 
         System.out.println();
         System.out.println("先手を選んでください(0:Base、1:User、2:Comp、3:Master)>");
-        int cho = in.nextInt();
-        switch (cho){
-            case 0 : basePlayers[0] = new BasePlayer();break;
-            case 1 : basePlayers[1] = new UserPlayer();break;
-            case 2 : basePlayers[2] = new CompPlayer();break;
-            case 3 : basePlayers[3] = new MasterPlayer();break;
-        }
+        int cho = in.nextInt();if(cho == 1) basePlayers1.add(new UserPlayer());
+        else basePlayers1.add(basePlayers[cho]);
+
         System.out.print("後手を選んでください(0:Base、1:User、2:Comp、3:Master)>");
-        int cho2 = in.nextInt();
-        switch (cho2){
-            case 0 : basePlayers[0] = new BasePlayer();break;
-            case 1 : basePlayers[1] = new UserPlayer();break;
-            case 2 : basePlayers[2] = new CompPlayer();break;
-            case 3 : basePlayers[3] = new MasterPlayer();break;
-        }
+        int cho2 = in.nextInt();if(cho2 == 1) basePlayers1.add(new UserPlayer());
+        else basePlayers1.add(basePlayers[cho2]);
 
         // 自己紹介
-//        user.intro();
-//        comp.intro();
-        basePlayers[cho].intro();
-        basePlayers[cho2].intro();
+        basePlayers1.get(0).intro();
+        basePlayers1.get(1).intro();
         System.out.println();
-
-        int take;				// 取る石の数
+        int take;   // 取る石の数
+        int i = 0;   //先頭と後手確認するやつ
         while(true) {
             // 先手の手番
             showStone(stone);
-            System.out.println("先手：" + basePlayers[cho].toString() + "の番です。");
-            take = basePlayers[cho].takeStone(stone);
+            System.out.println("先手：" + basePlayers1.get(i).toString() + "の番です。");
+            take = basePlayers1.get(i).takeStone(stone);
             System.out.println(take + "個取りました！");
             stone -= take;
             if (stone <= 0) {
-                System.out.println(basePlayers[cho].toString() + "の負けです！");
+                System.out.println(basePlayers1.get(i).toString() + "の負けです！");
                 break;								// while文を抜ける
             }
             System.out.println();
-
-            // 後手の手番
-            showStone(stone);
-            System.out.println("後手：" + basePlayers[cho2].toString() + "の番です。");
-            take = basePlayers[cho2].takeStone(stone);
-            System.out.println(take + "個取りました！");
-            stone -= take;
-            if (stone <= 0) {
-                System.out.println(basePlayers[cho2].toString()+ "の負けです！");
-                break;								// while文を抜ける
-            }
-            System.out.println();
+            if(i == 0) i ++; else i--; // 先、後交代
         }
     }
     public static void showStone(int stone) {
